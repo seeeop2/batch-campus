dcc283e9ab19cbd72043752638baea54a64ba254
SEEEOP2 <seeeop2@gmail.com>
2024-11-06 22:28:38 +0900

    CHORE: 프로젝트 환경 세팅 완료
    강의와 동일한 환경을 구성함.
    
    - SpringBoot: 3.1.0
    - Java: 17
    - Gradle: 8.10.2

---
+++ src/main/java/com/fastcampus/batchcampus/BatchCampusApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.batchcampus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchCampusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchCampusApplication.class, args);
+    }
+
+}
