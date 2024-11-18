6ee2ed32211d4f6df970210a0bb934d2fed0c169
SEEEOP2 <seeeop2@gmail.com>
2024-11-18 22:24:05 +0900

    REFACTOR: DormantBatchConfiguration에서 SimpleTasklet 사용
    - DormantBatchConfiguration 클래스에서 DormantBatchTasklet 대신 SimpleTasklet 사용
    - ItemReader, ItemProcessor, ItemWriter를 주입받아 SimpleTasklet 생성
    - 배치 작업의 구성 방식을 개선하여 코드의 일관성 및 재사용성 향상

--- src/main/java/com/fastcampus/batchcampus/application/DormantBatchConfiguration.java
+++ src/main/java/com/fastcampus/batchcampus/application/DormantBatchConfiguration.java
@@ -1,6 +1,7 @@
 package com.fastcampus.batchcampus.application;
 
 import com.fastcampus.batchcampus.batch.Job;
+import com.fastcampus.batchcampus.batch.SimpleTasklet;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,8 +10,16 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class DormantBatchConfiguration {
 
+    // 배치 작업을 정의하는 Job 빈 생성
     @Bean
-    public Job dormantBatchJob(DormantBatchTasklet dormantBatchTasklet, DormantBatchJobExecutionListener dormantBatchJobExecutionListener){
-        return new Job(dormantBatchTasklet, dormantBatchJobExecutionListener);
+    public Job dormantBatchJob(DormantBatchItemReader dormantBatchItemReader,
+                               DormantBatchItemProcessor dormantBatchItemProcessor,
+                               DormantBatchItemWriter dormantBatchItemWriter,
+                               DormantBatchJobExecutionListener dormantBatchJobExecutionListener){
+
+        // SimpleTasklet을 생성하여 ItemReader, ItemProcessor, ItemWriter를 주입
+        final SimpleTasklet tasklet = new SimpleTasklet(dormantBatchItemReader, dormantBatchItemProcessor, dormantBatchItemWriter);
+
+        return new Job(tasklet, dormantBatchJobExecutionListener); // 생성된 Tasklet과 JobExecutionListener를 사용하여 Job 객체 반환
     }
 }
